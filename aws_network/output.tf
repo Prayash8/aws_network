d85634d4f432bf24633cf2ead35027787d27a1c8
EC2 Default User <ec2-user@ip-172-31-8-172.ec2.internal>
2025-06-08 03:49:34 +0000

    Fixing variable definitions and resource names in network module

--- aws_network/output.tf
+++ aws_network/output.tf
@@ -2,7 +2,6 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.this.id
 }
-
 output "public_subnet_ids" {
   description = "List of IDs of public subnets"
   value       = aws_subnet.public.*.id
